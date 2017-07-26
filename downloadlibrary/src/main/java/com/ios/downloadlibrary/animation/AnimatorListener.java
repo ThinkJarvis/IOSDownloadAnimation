b354960133cf3dbb85a3dd88fe7f310914e676b7
weijiaqi <jiaqi.wei@gometech.com.cn>
2017-07-26 13:35:39 +0800

    remove old lib

--- downloadlibrary/src/main/java/com/ios/downloadlibrary/animation/AnimatorListener.java
+++
@@ -1,9 +0,0 @@
-package com.ios.downloadlibrary.animation;
-
-/**
- * Created by weijiaqi on 2017/7/23.
- */
-
-public interface AnimatorListener {
-    void onUpdateListener(float interpolator);
-}
